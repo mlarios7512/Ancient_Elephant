c684705b6efe3189a98d8799e1fe59095f3c761e
Michael Larios <lariosmichael98@gmail.com>
2023-12-12 14:43:46 -0800

    Wrapped printing of help commands into 'HelpMessages' class.

--- Ancient Elephant/Classes/Commands.cs
+++ Ancient Elephant/Classes/Commands.cs
@@ -1,5 +1,6 @@
 ﻿using Ancient_Elephant.Classes.Misc;
 using Ancient_Elephant.Classes.Misc.Messages;
+
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -59,52 +60,10 @@ namespace Ancient_Elephant.Classes
 
         public static void HelpCommand()
         {
-
-            //----------------------------------------------------
-            Console.WriteLine("\nHelp commands color key:");
-            PrettyConsole.WriteHelpKeyColor("White", ConsoleColor.White, "Normal text");
-            PrettyConsole.WriteHelpKeyColor("Yellow", ConsoleColor.DarkYellow, "Command");
-            PrettyConsole.WriteHelpKeyColor("Blue", ConsoleColor.Blue, "Option");
-            Console.WriteLine("\n\n");
-
-
-            //----------------------------------------------------
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("launch [csv file]");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Start processes from a CSV file. Files MUST be in CSV format (xlsx & xls are NOT supported).\n");
-
-            //----------------------------------------------------
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("clear");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Clears the console.\n");
-
-            //-------------------------------------------------
-            //Console.ForegroundColor = ConsoleColor.DarkYellow;
-            //Console.WriteLine("launchdir [directory path]");
-            //Console.ForegroundColor = ConsoleColor.White;
-            //Console.WriteLine("Change the path of the directory that is read to launch the CSV files from.\n");
-
-            //-----------------------------------------------
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("preferreddir [option]");
-            Console.ResetColor();
-            //List<(string, string)> options = new List<(string, string)>();
-            //(string, string) j = ("-view", "Show the current preferred directory.");
-            //(string, string) k = ("-view", "Show the current preferred directory.");
-
-            List<(string, string)> ops = new List<(string, string)>() 
-            {
-                ("view", "Show the current preferred directory."),
-                ("change", "Change the current preferred directory")
-            };
-
-
-
-            PrettyConsole.WriteOptionsForCommand(ops);
-
-
+            HelpMessages.ColorLegend();
+            HelpMessages.LaunchCommand();
+            HelpMessages.ClearConsoleCommand();
+            HelpMessages.ChangePrefDir();
 
             //END OF COMMANDS LISTING---------------------
             Console.WriteLine("\n");
---
+++ Ancient Elephant/Classes/Misc/Messages/HelpMessages.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ancient_Elephant.Classes.Misc.Messages
+{
+    public class HelpMessages
+    {
+        public static void ColorLegend() 
+        {
+            Console.WriteLine("\nHelp commands color key:");
+            PrettyConsole.WriteHelpKeyColor("White", ConsoleColor.White, "Normal text");
+            PrettyConsole.WriteHelpKeyColor("Yellow", ConsoleColor.DarkYellow, "Command");
+            PrettyConsole.WriteHelpKeyColor("Blue", ConsoleColor.Blue, "Option");
+            Console.WriteLine("\n\n");
+        }
+
+        public static void LaunchCommand()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("launch [csv file]");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Start processes from a CSV file. Files MUST be in CSV format (xlsx & xls are NOT supported).\n");
+        }
+
+        public static void ClearConsoleCommand()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("clear");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Clears the console.\n");
+        }
+
+        public static void ChangePrefDir()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("preferreddir [option]");
+            Console.ResetColor();
+            //List<(string, string)> options = new List<(string, string)>();
+            //(string, string) j = ("-view", "Show the current preferred directory.");
+            //(string, string) k = ("-view", "Show the current preferred directory.");
+
+            List<(string, string)> ops = new List<(string, string)>()
+            {
+                ("view", "Show the current preferred directory."),
+                ("change", "Change the current preferred directory")
+            };
+
+            PrettyConsole.DisplayOptionsForCommand(ops);
+        }
+
+    }
+}
